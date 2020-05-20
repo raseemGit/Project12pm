23902ecb823de6a285858aae823acace6d1f4baf
renit10 <reni.renny@gmail.com>
2020-05-20 20:01:03 +0530

    Push new code to Renit

--- src/test/java/com/task/Raseem.java
+++ src/test/java/com/task/Raseem.java
@@ -5,8 +5,10 @@ public class Raseem {
 	public static void main(String[] args) {
 
 		System.out.println("Raseem");
-		
-
+		System.out.println("Saranya");
+		System.out.println("Greens Technology");
+		System.out.println("Automation Testing");
+		System.out.println("Porur");
 	}
 
 }
